2bc95701cd843dad958f17a901486d84c9d8fb55
techkeshav23 <keshavupadhyay585@gmail.com>
2025-10-14 15:39:08 +0530

    hii

---
+++ app.jsx
@@ -0,0 +1,16 @@
+const parent = document.getElementById('parent');
+// console.log(parent)
+
+const root =ReactDOM.createRoot(parent);
+// const h2= React.createElement("h2",{id:"heading",style:{color:"white",textAlign:"center",fontSize:"50px"}},"ABES Engineering College");
+// const li1=React.createElement("li",{},"React");
+// const li2=React.createElement("li",{},"Java");
+// const li3=React.createElement("li",{},"C++");
+// const img =React.createElement("img",{src:"https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&fm=jpg&q=60&w=3000",style:{width:"500px"}});
+// const list =React.createElement("ul",{style:{backgroundColor:"black",height:"100vh",margin:"0px"}},[h2,li1,li2,li3,img]);
+// const div =React.createElement("ul",{},[h2,list]);
+// root.render(div);
+
+
+const h21 =<h2>hiii,i am using jsx</h2>
+root.render(h21);
